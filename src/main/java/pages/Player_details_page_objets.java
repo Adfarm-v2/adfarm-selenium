baace7634a2760adda5b9848ea0dabba54ad682d
Gopinath <Gopinath@gsche-e3108.kaaysystems.com>
2020-01-31 19:46:56 +0530

    Office new Branch commit for friday.

--- src/main/java/pages/Player_details_page_objets.java
+++ src/main/java/pages/Player_details_page_objets.java
@@ -1,7 +1,74 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Player_details_page_objets {
+
+	@FindBy(xpath = "//a[@href='/dashboard/player-list']")
+	public static WebElement myplayer;
 	
+	@FindBy(xpath = "//button[@type='button']")
+	public static WebElement newplayer;
+
+	@FindBy(xpath = "//input[@name='username']")
+	public static WebElement usernamefield;
+
+	@FindBy(xpath = "//input[@name='location_name']")
+	public static WebElement locationnamefield;
 	
+	@FindBy(xpath = "//div[@id='status_id']")
+	public static WebElement status;
+	@FindBy(xpath = "(//*[text()='Active'])[1]")
+	public static WebElement statustitle;
+
+	@FindBy(xpath = "//div[@id='distributor_id']")
+	public static WebElement distributor;
+	@FindBy(xpath = "//li[text()='Pro Digital']")
+	public static WebElement distributortitle;
+	
+	@FindBy(xpath = "//div[@id='dealer_id']")
+	public static WebElement dealer;
+	@FindBy(xpath = "//li[text()='Steady Brand']")
+	public static WebElement dealertitle;
+
+	@FindBy(xpath = "//div[@id='customer_id']")
+	public static WebElement customer;
+	@FindBy(xpath = "//li[text()='On Hold 32']")
+	public static WebElement customertitle;
+	
+	@FindBy(xpath = "//div[@id='location_id']")
+	public static WebElement location;
+	@FindBy(xpath = "//li[text()='Dental Office']")
+	public static WebElement locationtitle;
+	
+	@FindBy(xpath = "//div[text()='Please select Country']")
+	public static WebElement country;
+	@FindBy(xpath = "//li[text()='India']")
+	public static WebElement countrytitle;
+	
+	@FindBy(xpath = "//div[text()='Please select State']")
+	public static WebElement state;
+	@FindBy(xpath = "//li[text()='Tamil Nadu']")
+	public static WebElement statetitle;
+	
+	@FindBy(xpath = "//div[text()='Select City']")
+	public static WebElement city;
+	@FindBy(xpath = "//*[text()='Coimbatore']")
+	public static WebElement citytitle;
+	
+	@FindBy(xpath = "//div[text()='Please select Cluster']")
+	public static WebElement group;
+	@FindBy(xpath = "//*[text()='Chennai North']")
+	public static WebElement grouptitle;
+	
+	@FindBy(xpath = "//input[@name='zipcode']")
+	public static WebElement zipcode;
+
+	@FindBy(xpath = "//input[@placeholder='search']")
+	public static WebElement mapsearch;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	public static WebElement submit;
 
 }
--- src/main/java/testcases/Test_newplayer_creation.java
+++ src/main/java/testcases/Test_newplayer_creation.java
@@ -5,22 +5,61 @@ import org.testng.annotations.Test;
 
 import base.TestBase;
 import pages.Login_page_objects;
+import pages.Player_details_page_objets;
 
 public class Test_newplayer_creation extends TestBase {
 
 	public void login() {
 		PageFactory.initElements(driver, Login_page_objects.class);
 
-		Login_page_objects.uname.sendKeys(properties.getProperty("username") + System.currentTimeMillis());
+		Login_page_objects.uname.sendKeys(properties.getProperty("username"));
 		Login_page_objects.pwd.sendKeys(properties.getProperty("password"));
 		Login_page_objects.logbtn.click();
 
 	}
 
 	@Test
-	public void newplayercreation() {
+	public void newplayercreation() throws InterruptedException {
 
 		login();
+		PageFactory.initElements(driver, Player_details_page_objets.class);
+		Thread.sleep(3000);
+		Player_details_page_objets.newplayer.click();
+		Thread.sleep(3000);
+		Player_details_page_objets.usernamefield.sendKeys("GopiDist1Play4");
+		Player_details_page_objets.locationnamefield.sendKeys("cakes & bakes");
+		Thread.sleep(2000);
+		Player_details_page_objets.status.click();
+		Player_details_page_objets.statustitle.click();
+		
+		Player_details_page_objets.distributor.click();
+		Player_details_page_objets.distributortitle.click();
+		
+		Player_details_page_objets.dealer.click();
+		Player_details_page_objets.dealertitle.click();
+		
+		Player_details_page_objets.customer.click();
+		Player_details_page_objets.customertitle.click();
+		
+		Player_details_page_objets.location.click();
+		Player_details_page_objets.locationtitle.click();
+		Thread.sleep(2000);
+		Player_details_page_objets.country.click();
+		Player_details_page_objets.countrytitle.click();
+
+		Thread.sleep(2000);
+		Player_details_page_objets.state.click();
+		Player_details_page_objets.statetitle.click();
+
+		Player_details_page_objets.city.click();
+		Player_details_page_objets.citytitle.click();
+
+		Player_details_page_objets.group.click();
+		Player_details_page_objets.grouptitle.click();
+
+		
+		
+		Player_details_page_objets.zipcode.sendKeys("641041");
 		
 	}
 
