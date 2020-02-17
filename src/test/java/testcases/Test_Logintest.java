95615cf3ce42e853963d395fbcdec2da6d582fad
gopi <gopigopi033@gmail.com>
2020-02-17 21:45:59 +0530

    Testing Committed

--- src/test/java/testcases/Test_Logintest.java
+++ src/test/java/testcases/Test_Logintest.java
@@ -36,7 +36,7 @@ public class Test_Logintest extends TestBase {
 
 		FileInputStream m = new FileInputStream("C:\\Selenium Excel Sheet\\ERPTesting.xls");
 		Workbook excel = Workbook.getWorkbook(m);
-		Sheet sheet = excel.getSheet("Sheet1");
+		Sheet sheet = excel.getSheet("loginsheet");
 		int rowcount = sheet.getRows();
 		int columncount = sheet.getColumns();
 
